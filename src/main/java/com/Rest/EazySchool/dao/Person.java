2a7010df25537f2519c6f98c3224eecab7dbd3ac
ravenzzl <ravenzzco1@gmail.com>
2024-07-14 16:23:12 +0530

    person entity created and application properties added for mysql

---
+++ src/main/java/com/Rest/EazySchool/dao/Person.java
@@ -0,0 +1,26 @@
+package com.Rest.EazySchool.dao;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Person {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO,generator = "native")
+	@GenericGenerator(strategy = "native",name = "native")
+	private Long personId;
+	private String Name;
+	private String MobileNumber;
+	private String email;
+	private String password;
+
+}
